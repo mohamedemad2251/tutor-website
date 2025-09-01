636b09fa66322a37607195c45cc50b6ac3d24969
mohamedemad2251 <mohamedemad22521@gmail.com>
2025-09-01 19:29:01 +0300

    Added Sections Till "Key Takeaways" + Scrolling Header Visibility + Cloud SVG For Main Hero

--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const mobile_menu_toggle = document.querySelector('#mobile-menu-toggle-button');
     const mobile_canvas = document.querySelector('.mobile-canvas');
+
+    const header = document.querySelector('.header');
+
     mobile_menu_toggle.addEventListener('click', function () {
         mobile_canvas.classList.toggle('active');
+    })
+
+    document.addEventListener('scroll', function () {
+        // console.log(window.scrollY);
+        if (window.scrollY === 0) {
+            header.classList.remove('scroll');
+        }
+        else {
+            header.classList.add('scroll')
+        }
     })
 })
